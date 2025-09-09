dd44961162c0aa61ea177cc9cd315bf5d2d16035
towny3 <nicholas.townshend3@gmail.com>
2025-09-09 10:57:41 +1000

    implemented the Node interface for the Variable class

--- src/main/java/Nodes/Variable.java
+++ src/main/java/Nodes/Variable.java
@@ -1,7 +1,31 @@
 package Nodes;
 
+import java.util.Optional;
+
 /**
  * Represents a single algebraic variable
  */
-public class Variable {
+public class Variable implements Node {
+    private final String identifier;
+
+    public Variable(String identifier) {
+        this.identifier = identifier;
+    }
+
+    @Override
+    public Optional<Node> evaluate(Node var, Node input) {
+        // if the variable matches
+        if (var.equals(this)) {
+            // replace the instance of the variable with the input provided
+            return Optional.of(input);
+        }
+
+        // otherwise, continue with just the original variable
+        return Optional.of(this);
+    }
+
+    @Override
+    public String toLatex() {
+        return identifier;
+    }
 }
