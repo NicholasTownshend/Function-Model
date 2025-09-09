75216e3090aecf624c67d4a055808ca01c4c950b
towny3 <nicholas.townshend3@gmail.com>
2025-09-09 10:54:08 +1000

    added more commenting and javadocs and changed accessibility of properties

--- src/main/java/Nodes/Constant.java
+++ src/main/java/Nodes/Constant.java
@@ -3,8 +3,13 @@ package Nodes;
 import java.util.Optional;
 
 public class Constant implements Node {
-    double constant;
+    // the value of the number represented by the constant
+    private final double constant;
 
+    /**
+     * Constructor for the constant
+     * @param constant the value of the constant being represented
+     */
     public Constant(double constant) {
         this.constant = constant;
     }
--- src/main/java/Nodes/Node.java
+++ src/main/java/Nodes/Node.java
@@ -3,6 +3,17 @@ package Nodes;
 import java.util.Optional;
 
 public interface Node {
+    /**
+     * Evaluates the node
+     * @param var the node to be assigned the value of input
+     * @param input the input for the variable
+     * @return the resulting expression produced by the algebraic input
+     */
     Optional<Node> evaluate(Node var, Node input);
+
+    /**
+     * Method for getting the latex representation of a node
+     * @return string of the latex representation of this node
+     */
     String toLatex();
 }
