84145c5574eb43376943b8bdc796d7f02556b37e
towny3 <nicholas.townshend3@gmail.com>
2025-09-16 13:21:21 +1000

    created Token and TokenType

---
+++ src/main/java/parsing/tokens/Token.java
@@ -0,0 +1,11 @@
+package parsing.tokens;
+
+public class Token {
+    private TokenType type;
+    private String value;
+
+    public Token(TokenType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
---
+++ src/main/java/parsing/tokens/TokenType.java
@@ -0,0 +1,7 @@
+package parsing.tokens;
+
+public enum TokenType {
+    WORD,
+    NUMBER,
+    SYMBOL
+}
